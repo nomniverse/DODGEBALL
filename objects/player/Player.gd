1a9727d9ff217104977a2c698a0c223e383489be
Nam Tran <tranngocnam97@gmail.com>
2020-04-30 04:00:08 -0400

    Added two players and kinda working tagging

--- objects/player/Player.gd
+++ objects/player/Player.gd
@@ -6,6 +6,8 @@ const JUMP_FORCE     = 1000
 const GRAVITY        = 50
 const MAX_FALL_SPEED = 1000
 
+export var player_id : int = 0
+
 # == Node References ==
 onready var anim_player = $AnimationPlayer
 onready var sprite = $Sprite
@@ -13,18 +15,40 @@ onready var sprite = $Sprite
 # == Member Variables ==
 var y_velo = 0
 var facing_right = false
+var is_jumping = false
+var is_attacking = false
 
 func _physics_process(delta):
 	var move_dir = 0
-	if Input.is_action_pressed("move_right"):
-		move_dir += 1
-	if Input.is_action_pressed("move_left"):
-		move_dir -= 1
+	
+	if player_id == 0:
+		if Input.is_action_pressed("player_one_move_right"):
+			move_dir += 1
+		if Input.is_action_pressed("player_one_move_left"):
+			move_dir -= 1
+		
+		is_jumping = Input.is_action_just_pressed("player_one_jump")
+		
+		is_attacking = Input.is_action_pressed("player_one_tag")
+	elif player_id == 1:
+		if Input.is_action_pressed("player_two_move_right"):
+			move_dir += 1
+		if Input.is_action_pressed("player_two_move_left"):
+			move_dir -= 1
+		
+		is_jumping = Input.is_action_just_pressed("player_two_jump")
+		
+		is_attacking = Input.is_action_pressed("player_two_tag")
+	else:
+		pass
+		
 	move_and_slide(Vector2(move_dir * MOVE_SPEED, y_velo), Vector2(0, -1))
    
 	var grounded = is_on_floor()
+	
 	y_velo += GRAVITY
-	if grounded and Input.is_action_just_pressed("jump"):
+	
+	if grounded and is_jumping:
 		y_velo = -JUMP_FORCE
 	if grounded and y_velo >= 5:
 		y_velo = 5
@@ -35,29 +59,41 @@ func _physics_process(delta):
 		flip()
 	if !facing_right and move_dir > 0:
 		flip()
-   
+
 	if grounded:
-		if move_dir == 0:
-			play_anim("idle")
+		if is_attacking:
+			play_anim("attack")
+			is_attacking = false
 		else:
-			play_anim("walk")
+			if move_dir == 0:
+				play_anim("idle")
+			else:
+				play_anim("walk")
 	else:
 		play_anim("jump")
  
 func flip():
 	facing_right = !facing_right
-	sprite.flip_h = !sprite.flip_h
+	sprite.scale.x = -sprite.scale.x
  
 func play_anim(anim_name):
 	if anim_player.is_playing() and anim_player.current_animation == anim_name:
 		return
+	
 	anim_player.play(anim_name)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_TagHit_body_entered(body):
+	if not body.get_name() == self.get_name():
+		if body.get_name().begins_with("Player"):
+			print("Tag! You're whatever we do with this...")
+	
+		
